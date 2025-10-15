c9968bb4a1878793a11c0ffa6acb76e079a6e88d
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2025-10-15 14:09:52 +0600

    Add files via upload

---
+++ Module-1 | Contest-1: Data types, Variables, Operators/L_Food_Costs.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    int x,y;
+
+    cin>>x>>y;
+
+    int totalCost=(6*x)+y;
+
+    cout<<totalCost<<endl;
+
+    return 0;
+}
---
+++ Module-1 | Contest-1: Data types, Variables, Operators/M_Bone_Appetit.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    int n,m;
+
+    int x,y;
+
+    cin>>n>>m;
+
+    cin>>x>>y;
+
+    int total;
+
+    total=(n*x)+(m*y);
+
+    cout<<total<<endl;
+
+    return 0;
+
+    
+}
