b8c7dcfac72d6d5d13b63e9d8bbb577e40495c3d
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2025-10-15 14:10:44 +0600

    Add files via upload

---
+++ Module-1 | Contest-1: Data types, Variables, Operators/P_Rectangle.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    int a,b;
+    cin>>a>>b;
+
+    int area;
+
+    area=a*b;
+
+    int perimeter;
+
+    perimeter=2*(a+b);
+
+    cout<<area<<" "<<perimeter<<endl;
+
+    return 0;
+}
---
+++ Module-1 | Contest-1: Data types, Variables, Operators/R_Devouring_Donuts.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    int x,y;
+    cin>>x>>y;
+
+    int total;
+
+    total=x*y;
+
+    cout<<total<<endl;
+
+    return 0;
+    
+}
