32664b981daef4d02c68885ec9b19c3f972c5b1b
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2025-10-23 15:47:13 +0600

    Add files via upload

---
+++ Module 1/Module-1 | Contest-2: Conditional Statement/I_Fan_Poll.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int dhoni,rohit,kohli;
+
+    cin>>dhoni>>rohit>>kohli;
+
+    if(dhoni>rohit && dhoni>kohli)
+    {
+      cout<<"Yes"<<endl;
+    }
+
+    else{
+        cout<<"No"<<endl;
+    }
+
+    return 0;
+}
