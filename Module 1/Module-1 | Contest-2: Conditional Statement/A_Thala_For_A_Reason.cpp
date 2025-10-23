ddebd127a08ef0f9158f3677d9fd0e70995254d0
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2025-10-23 14:21:57 +0600

    Create A_Thala_For_A_Reason.cpp

---
+++ Module 1/Module-1 | Contest-2: Conditional Statement/A_Thala_For_A_Reason.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int x;
+    cin>> x;
+
+    if(x==7){
+        cout<<"THALA"<<endl;
+    }
+    else
+    {
+        cout<<"SADGE"<<endl;
+    }
+    
+    return 0;
+}
