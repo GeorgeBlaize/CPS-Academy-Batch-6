1dc15fccb28c1774dd839b0f93dadd91412e099d
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2025-10-23 14:50:56 +0600

    Add files via upload

---
+++ Module 1/Module-1 | Contest-2: Conditional Statement/E_Minimum_Wage.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    
+    int minimumWage;
+
+    cin>>minimumWage;
+
+    if(minimumWage>11){
+        cout<<"YES"<<endl;
+    }
+
+    else{
+        cout<<"NO"<<endl;
+    }
+    
+    return 0;
+}
