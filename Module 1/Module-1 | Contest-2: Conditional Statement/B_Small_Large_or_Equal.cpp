c26ae4d21097083a0b9b29c513e46328325c4b7d
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2025-10-23 14:32:39 +0600

    Add files via upload

---
+++ Module 1/Module-1 | Contest-2: Conditional Statement/B_Small_Large_or_Equal.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    
+    int num1,num2;
+
+    cin>>num1>>num2;
+
+    if(num1>num2)
+    {
+        cout<<"a > b"<<endl;
+    }
+    else if(num1<num2){
+        cout<<"a < b"<<endl;
+    }
+    else
+    {
+        cout<<"a == b"<<endl;
+    }
+
+
+    return 0;
+}
