2920edcf6d96a80791aaf89f1bb8a905d7aeb9d0
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2025-10-23 15:23:05 +0600

    Add files via upload

---
+++ Module 1/Module-1 | Contest-2: Conditional Statement/G_Cricket_World_Cup_Qualifier.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int qualifierTeam;
+    cin>>qualifierTeam;
+    
+    if(qualifierTeam>=12){
+        cout<<"Yes"<<endl;
+    }
+    else{
+        cout<<"No"<<endl;
+    }
+
+    return 0;
+}
