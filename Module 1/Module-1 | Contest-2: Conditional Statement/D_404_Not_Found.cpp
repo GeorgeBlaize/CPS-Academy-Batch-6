e56b9ccff262a0897b01667d32d35f22fbab1a57
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2025-10-23 14:50:29 +0600

    Add files via upload

---
+++ Module 1/Module-1 | Contest-2: Conditional Statement/D_404_Not_Found.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int num;
+
+    cin>>num;
+
+    if(num==404){
+        cout<<"NOT FOUND"<<endl;
+    }
+
+    else{
+        cout<<"FOUND"<<endl;
+    }
+
+
+    return 0;
+}
