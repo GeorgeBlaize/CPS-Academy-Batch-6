08ca38afe1dda9831cc0fab6d1dac73417342845
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2025-10-24 13:03:03 +0600

    Add files via upload

---
+++ Module 1/Module-1 | Contest-2: Conditional Statement/M_Air_Quality_Index.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int aqi;
+    cin>>aqi;
+
+    if(aqi<100){
+        cout<<"YES"<<endl;
+    }
+
+    else if(aqi>=100){
+        cout<<"NO"<<endl;
+    }
+
+    
+    return 0;
+
+}
