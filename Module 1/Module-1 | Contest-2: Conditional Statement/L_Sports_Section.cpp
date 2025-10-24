52f81e6119e58e7603434d6ccb2938651d931752
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2025-10-24 12:39:37 +0600

    Add files via upload

---
+++ Module 1/Module-1 | Contest-2: Conditional Statement/L_Sports_Section.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int page;
+    cin>>page;
+
+    if(page>=1 && page<=7){
+        cout<<"NO"<<endl;
+    }
+    else if(page>7){
+        cout<<"YES"<<endl;
+    }
+    
+    return 0;
+}
