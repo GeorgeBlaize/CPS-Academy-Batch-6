3b32c06ace530ff7a512360c0c9d1ba5ff558118
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2025-10-24 13:07:25 +0600

    Add files via upload

---
+++ Module 1/Module-1 | Contest-2: Conditional Statement/N_Puzzle_Hunt.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int team;
+    cin>>team;
+
+    if(team>=6 && team<=8){
+        cout<<"Yes"<<endl;
+    }
+
+    else{
+        cout<<"No"<<endl;
+    }
+    return 0;
+}
