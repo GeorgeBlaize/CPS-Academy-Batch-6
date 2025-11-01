154356dab95bd538533496b315cac8712e858c02
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2025-11-02 01:08:42 +0600

    Create A_A.cpp

---
+++ Module 1/Module-1 | Contest-3: Loops/A_A.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    
+    int n;
+    
+    cin>>n;
+
+    while (n--)
+    {
+        int age;
+
+        cin>>age;
+
+        cout<<age-10<<endl;
+
+
+    }
+    
+
+
+    return 0;
+}
