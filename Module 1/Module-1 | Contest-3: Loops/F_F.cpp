2ce26bf1f54f0661cdb005e3f1b261dec55c51d4
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2025-11-03 11:33:08 +0600

    Add files via upload

---
+++ Module 1/Module-1 | Contest-3: Loops/F_F.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    
+    int num;
+    cin>>num;
+    
+    cout<<"L";
+    for(int i=0; i<num; i++){
+        cout<<"o";
+    }
+
+    cout<<"ng"<<endl;
+    
+    
+    return 0;
+}
