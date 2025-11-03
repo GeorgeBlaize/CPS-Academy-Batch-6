c39026f8dff9943e29436a65f2d88461352fa12b
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2025-11-03 13:17:01 +0600

    Add files via upload

---
+++ Module 1/Module-1 | Contest-3: Loops/O_O.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    
+    int t;
+    cin>>t;
+
+    while (t--)
+    {
+       int s;
+       cin>>s;
+       int eachFrameWorth=s*1000;
+       
+       int video=24;
+
+       int total=video*eachFrameWorth;
+
+       cout<<total<<endl;
+
+    }
+    
+
+    return 0;
+}
