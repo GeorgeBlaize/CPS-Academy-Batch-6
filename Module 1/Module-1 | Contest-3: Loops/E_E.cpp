8960f65544270d49a3103b6e84af996655906b7a
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2025-11-03 11:32:49 +0600

    Add files via upload

---
+++ Module 1/Module-1 | Contest-3: Loops/E_E.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    
+    int t;
+    cin>>t;
+
+    while (t--)
+    {
+        
+        int temperature;
+        cin>>temperature;
+
+        if(temperature>24){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+    
+    return 0;
+}
