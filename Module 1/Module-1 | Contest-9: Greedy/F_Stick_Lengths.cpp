82d96666a57bdb2f5507f89a737a2d68e3d1f910
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2026-01-12 01:13:13 +0600

    Add files via upload

---
+++ Module 1/Module-1 | Contest-9: Greedy/F_Stick_Lengths.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    int n;
+    cin >> n;
+
+    vector<long long> p(n);
+    for (int i = 0; i < n; i++) {
+        cin >> p[i];
+    }
+
+    sort(p.begin(), p.end());
+
+    long long median = p[n / 2];
+    long long cost = 0;
+
+    for (int i = 0; i < n; i++) {
+        cost += llabs(p[i] - median);
+    }
+
+    cout << cost << "\n";
+    return 0;
+}
