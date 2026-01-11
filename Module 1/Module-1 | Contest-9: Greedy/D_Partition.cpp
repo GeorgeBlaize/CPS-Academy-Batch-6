e8ff893e79595b34a4e9212b27b53b73c84a875f
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2026-01-12 01:06:20 +0600

    Add files via upload

---
+++ Module 1/Module-1 | Contest-9: Greedy/D_Partition.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    
+    int n;
+    cin>>n;
+
+    long long ans=0;
+
+    for(int i=0; i<n; i++){
+        int x;
+        cin>>x;
+        ans+=abs(x);
+    }
+
+
+    cout<<ans<<endl;
+
+    return 0;
+}
