0eb5ff1fde00578eae79bf1cc742717c0d9da1da
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2026-02-09 18:14:05 +0600

    Create A_Oneful_Pairs.cpp

---
+++ Module 2/STL | Contest - 4: Pair/A_Oneful_Pairs.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    
+    pair<int,int> p;
+    cin>>p.first>>p.second;
+    if(p.first+p.second+(p.first*p.second)==111){
+        cout<<"Yes"<<endl;
+    }
+    else{
+        cout<<"No"<<endl;
+    }
+
+    
+    return 0;
+}
